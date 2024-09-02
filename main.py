0305e78fc03f2288c53ecf9342e83516e303e311
Jaithra18 <Jaithrathota@gmail.com>
2024-09-02 18:26:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+a=10
+print(type(a))
+b=10.5
+print(type(b))
+c=True
+print(type(c))
+d="Hello"
+print(type(d))
+e=int(input("Enter a number"))
+print(type(e))
+f="I am learning python"
+print(f[2:3+1])
+print(f[::-1])
